9f8115d178a23d0627724686c223db2fa151b40a
lybell <32293736+lybell-art@users.noreply.github.com>
2021-05-01 02:19:36 +0900

    Update reveal.js

--- reveal.js
+++ reveal.js
@@ -151,7 +151,7 @@ function draw()
 	
 	ambientLight(50);
 	directionalLight(200,200,200,-1,1,-1);
-	let mousePos=myCam.screenTo3DRevolve(mouseX - windowWidth/2,mouseY - windowHeight/2,0.7,0.2);
+	let mousePos=myCam.screenTo3DRevolve(mouseX - windowWidth/2,mouseY - windowHeight/2,1,0.1);
 //	pointLight(255,255,255,mousePos);
 	
 	for(let i=0;i<lightArr.length;i++)
@@ -172,7 +172,7 @@ function draw()
 
 function mousePressed()
 {
-	let mousePos=myCam.screenTo3DRevolve(mouseX - windowWidth/2,mouseY - windowHeight/2,0.7,0.2);
+	let mousePos=myCam.screenTo3DRevolve(mouseX - windowWidth/2,mouseY - windowHeight/2,1,0.1);
 	lightArr.push(mousePos);
 }
 function windowResized()
